709a2dcfe1eeaba08839bddd4cc300e514319093
five3seven <radu.lupascu6@gmail.com>
2021-07-28 14:35:21 +0300

    charts

---
+++ src/components/Charts/LineChart.js
@@ -0,0 +1,190 @@
+import React from "react";
+import Chart from "react-apexcharts";
+import { useState, useEffect } from "react";
+
+export default function LineChart(props) {
+  const [options, setOptions] = useState({
+    chart: {
+      background: "transparent",
+      foreColor: "#333",
+      toolbar: {
+        show: false,
+      },
+      zoom: {
+        enabled: false,
+      },
+      animations: {
+        enabled: true,
+        easing: "easeinout",
+        speed: 800,
+        animateGradually: {
+          enabled: true,
+          delay: 150,
+        },
+        dynamicAnimation: {
+          enabled: true,
+          speed: 350,
+        },
+      },
+    },
+    legend: {
+      show: false,
+    },
+    grid: {
+      borderColor: "#E2E8F0",
+      strokeDashArray: 3,
+    },
+    colors: ["#4FD1C5", "#2D3748"], // Line color
+    xaxis: {
+      axisTicks: {
+        show: false,
+      },
+      axisBorder: {
+        show: false,
+      },
+      categories: [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+      ],
+      labels: {
+        style: {
+          colors: [
+            "#CBD5E0",
+            "#CBD5E0",
+            "#CBD5E0",
+            "#CBD5E0",
+            "#CBD5E0",
+            "#CBD5E0",
+            "#CBD5E0",
+            "#CBD5E0",
+            "#CBD5E0",
+            "#CBD5E0",
+            "#CBD5E0",
+            "#CBD5E0",
+          ],
+          fontSize: "12px",
+          fontFamily: "Helvetica, Arial, sans-serif",
+          fontWeight: 500,
+          cssClass: "apexcharts-xaxis-label",
+        },
+      },
+      title: {
+        text: "in 2021",
+        offsetX: "0rem",
+        offsetY: "0rem",
+        style: {
+          color: "#A0AEC0",
+          fontSize: "12px",
+          fontFamily: "Helvetica, Arial, sans-serif",
+          fontWeight: 450,
+          cssClass: "apexcharts-yaxis-title",
+        },
+      },
+    },
+    yaxis: {
+      labels: {
+        style: {
+          colors: ["#CBD5E0"],
+          fontSize: "12px",
+          fontFamily: "Helvetica, Arial, sans-serif",
+          fontWeight: 500,
+          cssClass: "apexcharts-yaxis-label",
+        },
+      },
+    },
+    fill: {
+      type: "gradient",
+      gradient: {
+        shade: "light",
+        type: "vertical",
+        shadeIntensity: 0.5,
+        gradientToColors: undefined, // optional, if not defined - uses the shades of same color in series
+        inverseColors: true,
+        opacityFrom: 0.8,
+        opacityTo: 0,
+        stops: [],
+      },
+    },
+    stroke: {
+      curve: "smooth",
+      width: 2,
+    },
+    title: {
+      text: undefined, // Sales overview
+      align: "left",
+      margin: 30,
+      offsetX: 10,
+      offsetY: 20,
+      style: {
+        fontSize: "14px",
+        color: "#2D3748",
+      },
+    },
+    subtitle: {
+      text: "in 2021",
+      align: "left",
+      margin: 30,
+      offsetX: 11,
+      offsetY: 37,
+      floating: false,
+      style: {
+        fontSize: "14px",
+        fontWeight: "500",
+        fontFamily: undefined,
+        color: "#CBD5E0",
+      },
+    },
+  });
+
+  const [series, setSeries] = useState(props.series);
+
+  useEffect(() => {
+    setSeries(props.series);
+  }, [props.series]);
+
+  useEffect(() => {
+    const inc = calculateIncrease();
+    let col,
+      txt,
+      sInc = Math.abs(inc).toString();
+    if (inc === 0) {
+      col = "#A0AEC0";
+      txt = txt = "(+" + sInc + "%) more";
+    } else {
+      inc > 0 ? (col = "#48BB78") : (col = "#E53E3E");
+      inc > 0
+        ? (txt = "(+" + sInc + "%) more")
+        : (txt = "(-" + sInc + "%) less");
+    }
+    setOptions({
+      ...options,
+      title: {
+        ...options.title,
+        text: txt,
+        style: {
+          ...options.title.style,
+          color: col,
+        },
+      },
+    });
+  }, [series]);
+
+  const calculateIncrease = () => {
+    const newNumber = series[0].data[series[0].data.length - 1];
+    const originalNumber = series[0].data[0];
+    const Increase = ((newNumber - originalNumber) / originalNumber) * 100;
+    return Math.floor(Increase);
+  };
+
+  return <Chart options={options} series={series} width="100%" height="445" />;
+}
--- src/views/Dashboard/DashboardChakra.js
+++ src/views/Dashboard/DashboardChakra.js
@@ -19,6 +19,8 @@ import CardHeader from "components/CardChakra/CardHeader.js";
 import CardIcon from "components/CardChakra/CardIcon.js";
 import CardBody from "components/CardChakra/CardBody.js";
 import CardFooter from "components/CardChakra/CardFooter.js";
+import BarChart from "components/Charts/BarChart";
+import LineChart from "components/Charts/LineChart";
 
 import {
   BuildIcon,
@@ -40,13 +42,90 @@ import {
   SpotifyLogo,
   InvisionLogo,
 } from "components/Icons/Icons.js";
-import IconBox from "components/Icons/IconBox.js";
+import { useState } from "react";
+import IconBox from "components/Icons/IconBox";
 
 export default function DashboardChakra() {
   const { colorMode, toggleColorMode } = useColorMode();
   const iconTeal = useColorModeValue("teal.300", "teal.300");
   const iconBoxInside = useColorModeValue("white", "white");
+  const [series, setSeries] = useState([
+    {
+      type: "area",
+      name: "Mobile apps",
+      data: [190, 220, 205, 350, 370, 450, 400, 360, 210, 250, 292, 190],
+    },
+    {
+      type: "area",
+      name: "Websites",
+      data: [400, 291, 121, 117, 25, 133, 121, 211, 147, 25, 201, 203],
+    },
+  ]);
 
+  const [data, setData] = useState([
+    {
+      data: [
+        {
+          x: "Jan",
+          y: [0, 75],
+        },
+        {
+          x: "Feb",
+          y: [0, 96],
+        },
+        {
+          x: "Mar",
+          y: [0, 200],
+        },
+        {
+          x: "Apr",
+          y: [0, 112],
+        },
+        {
+          x: "May",
+          y: [0, 53],
+        },
+        {
+          x: "Jun",
+          y: [0, 67],
+        },
+        {
+          x: "Jul",
+          y: [0, 108],
+        },
+        {
+          x: "Aug",
+          y: [0, 122],
+        },
+        {
+          x: "Sep",
+          y: [0, 74],
+        },
+        {
+          x: "Oct",
+          y: [0, 60],
+        },
+        {
+          x: "Nov",
+          y: [0, 81],
+        },
+        {
+          x: "Dec",
+          y: [0, 225],
+        },
+      ],
+    },
+  ]);
+  const GenerateSeries = () => {
+    const newSeries = [];
+    series.map((s) => {
+      const data = s.data.map(() => {
+        return Math.round(Math.random() * (180 - Math.exp(Math.random())));
+      });
+      newSeries.push({ data, name: s.name });
+    });
+    setSeries(newSeries);
+  };
   return (
     <VStack>
       <HStack>
@@ -63,24 +142,9 @@ export default function DashboardChakra() {
         </Button>
       </HStack>
       <SimpleGrid minChildWidth="120px" spacing="24px" width={"100%"}>
-        <Flex height="80px">
-          <Card>
-            <AtlassianLogo h={"32px"} w={"32px"} />
-          </Card>
-        </Flex>
-        <Flex height="80px">
-          <Card>
-            <SpotifyLogo h={"32px"} w={"32px"} />
-          </Card>
-        </Flex>
-        <Flex height="80px">
-          <Card>
-            <InvisionLogo h={"32px"} w={"32px"} />
-          </Card>
-        </Flex>
-        <Flex height="80px">
-          <Card>
-            <JiraLogo h={"32px"} w={"32px"} />
+        <Flex>
+          <Card height="800">
+            <LineChart series={series} />
           </Card>
         </Flex>
       </SimpleGrid>
